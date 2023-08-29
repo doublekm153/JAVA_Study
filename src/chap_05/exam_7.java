6ac68054fbe0cd252c61c6ae720cec50ed6f7b8a
doublekm153 <doublekm153@gmail.com>
2023-08-30 04:32:09 +0900

    chap_05 문제

---
+++ src/chap_05/exam_7.java
@@ -0,0 +1,16 @@
+package chap_05;
+
+public class exam_7 {
+    public static void main(String[] args) {
+        int max = 0;
+        int[] array = {1, 5, 3, 8, 2};
+
+        /*작성 위치
+        * 주어진 배열의 항목에서 최대값을 구해보세요(for문 이용)
+        * 실행결과
+        * max : 8*/
+
+        System.out.println("max : " + max);
+
+    }
+}
