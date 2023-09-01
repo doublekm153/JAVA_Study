a607d0ebbc7693b7924263a7969b5d8f9099d413
doublekm153 <doublekm153@gmail.com>
2023-09-02 02:07:02 +0900

    chap_08 기본 예제, 확인문제

---
+++ src/chap_08/Cat.java
@@ -0,0 +1,8 @@
+package chap_08;
+
+public class Cat implements Soundable {
+    @Override
+    public String sound() {
+        return "야옹";
+    }
+}
---
+++ src/chap_08/Dog.java
@@ -0,0 +1,8 @@
+package chap_08;
+
+public class Dog implements Soundable {
+    @Override
+    public String sound() {
+        return "멍멍";
+    }
+}
