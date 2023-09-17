115ffebc568e16a9fdc2789bfbc73d5d141bcc01
doublekm153 <doublekm153@gmail.com>
2023-09-18 02:58:44 +0900

    chap_010_예외 처리 기본 예제, 확인문제 try-catch-finally, throws, throw

---
+++ src/chap_10/NotExistIDException.java
@@ -0,0 +1,8 @@
+package chap_10;
+
+public class NotExistIDException extends Exception{
+    public NotExistIDException() {}
+    public NotExistIDException(String message) {
+        super(message);
+    }
+}
